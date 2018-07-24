96ca999f4fe8c2c62a84658db1767df7734d3422
MukulPatil123 <aniket.khara@wgblindia.com>
2018-07-24 20:42:19 +0530

    chapter 7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
   #get 'users/new'
   root 'static_pages#home'
-  #get 'static_pages/home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/help', to: 'static_pages#help'
-  #get  '/signup', to: 'static_pages#signup'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   get  '/login', to: 'static_pages#login'
-  #get 'static_pages/help'
-  #get 'static_pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
