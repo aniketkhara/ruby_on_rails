1011d46b094bb6848c46ccbf1f0a0a7fa96e0869
MukulPatil123 <aniket.khara@wgblindia.com>
2018-07-24 22:31:19 +0530

    chapter 8

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   #get 'users/new'
   root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
@@ -7,6 +8,9 @@ Rails.application.routes.draw do
   get  '/help', to: 'static_pages#help'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  get  '/login', to: 'static_pages#login'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  #get  '/login', to: 'static_pages#login'
   resources :users
 end
