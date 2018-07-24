bc46c5d398c85d438290f8c3d0a9357a9da0c977
MukulPatil123 <aniket.khara@wgblindia.com>
2018-07-24 17:19:24 +0530

    chapter 5

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   root 'static_pages#home'
   #get 'static_pages/home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/help', to: 'static_pages#help'
-  get  '/signup', to: 'static_pages#signup'
+  #get  '/signup', to: 'static_pages#signup'
+  get  '/signup',  to: 'users#new'
   get  '/login', to: 'static_pages#login'
   #get 'static_pages/help'
   #get 'static_pages/about'
