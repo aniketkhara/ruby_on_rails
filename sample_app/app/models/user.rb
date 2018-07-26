188168d5a5fc2acceff1b79197ffd01398de92d1
MukulPatil123 <aniket.khara@wgblindia.com>
2018-07-26 20:46:43 +0530

    chapter 14

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   #before_save { email.downcase! }
   #has_many :microposts
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -67,7 +76,26 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+                    following_ids: following_ids, user_id: id)
+     #Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    #Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    #following << other_user
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+    #active_relationships.delete(followed_id: other_user.id)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -14,8 +14,15 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   #get  '/login', to: 'static_pages#login'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  #resources :users
+  resources :account_activations, only: [:edit]
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
